d535d587c43eb080a550c5f8ff4f3ef06f081b5d
mintcnn <mintchnrt@gmail.com>
2020-11-10 10:51:54 +0700

    initial order page

--- bcoffee/src/index.js
+++ bcoffee/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import 'antd/dist/antd.css';
 import Bar from './component/bar'
 
 ReactDOM.render(
   <BrowserRouter>
-    <Bar />
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
