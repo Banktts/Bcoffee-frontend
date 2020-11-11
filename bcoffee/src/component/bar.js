377ff95c51faae98989971cda6389a729149d370
mintcnn <mintchnrt@gmail.com>
2020-11-11 23:38:09 +0700

    Add mock data and button in cart

--- bcoffee/src/component/bar.js
+++ bcoffee/src/component/bar.js
@@ -15,7 +15,7 @@ const Bar = () => {
             </div>
             <Menu mode="inline" defaultSelectedKeys="1" >
                 <Menu.Item key="1" >
-                    <Link to="/">Order</Link>
+                    <Link to="/order">Order</Link>
                 </Menu.Item>
                 <Menu.Item key="2" >
                     <Link to="/inventory">Inventory</Link>
