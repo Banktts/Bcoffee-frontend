71d428ebb3447425a64a372ae1973d62fed52b6d
Thanabodee Thamasaroch <bankthamasaroch@gmail.com>
2020-11-20 04:30:50 +0700

    style modal cusomer add

--- bcoffee/src/customer/customerAdd.js
+++ bcoffee/src/customer/customerAdd.js
@@ -2,75 +2,61 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { Modal, Form, Input, Button } from 'antd'
 import { CustomInput } from '../component/customInput'
 import { CustomSelect } from '../component/customSelect'
-import { showConfirm } from '../component/customModal'
 import { CustomDatepicker } from '../component/customDatepicker'
 import { addCustomer } from './../service/user.service'
 import { useHistory } from "react-router-dom";
+import { AlertTwoTone, ExclamationCircleOutlined, CheckCircleTwoTone, WarningTwoTone } from '@ant-design/icons';
 import './customerAdd.scss'
 const CustomerAdd = () => {
     const history = useHistory();
     const [form] = Form.useForm();
     const gender = ["Men", "Women"]
-    const [visible, setVisible] = useState(false);
-    const [confirmLoading, setConfirmLoading] = useState(false);
     const [values, setValues] = useState({});
-    const { confirm } = Modal;
-
+    const { confirm, info } = Modal;
 
     function showConfirm() {
         confirm({
-          title: 'Do you Want to delete these items?',
-          visible: {visible},
-          content: 'Some descriptions',
-          onOk() {
-            return new Promise(async(resolve, reject) => {
-                let st=await addCustomer(values);
-                console.log(st)
-                st=true;
-                setTimeout(st ? resolve(history.push('/customer')) : reject(console.log("ops")), 1000);
-              }).catch(() => console.log('Oops errors!'));
-          },
-          onCancel() {
-            console.log('Cancel');
-          },
+            title: 'Add Employee',
+            icon: <CheckCircleTwoTone twoToneColor="#52c41a" />,
+            content: 'Are you sure to add new employee.',
+            onOk() {
+                return new Promise(async(resolve, reject) => {
+                    let st = await addCustomer(values)
+                    if (st == true) {
+                        resolve(history.push('/customer'))
+                    } else if (st == false) {
+                        resolve(showInfo())
+                    }
+                })
+            },
+            onCancel() {
+                console.log('Cancel');
+            },
         });
-      }
+    }
 
-    const showModal = () => {
-        setVisible(true);
-    };
+    function showInfo() {
+        info({
+            title: 'Add Employee',
+            icon: <ExclamationCircleOutlined />,
+            content: 'Opps! Customer Id cannot duplicate.',
+            onOk() {
+            }
+        });
+    }
 
-    
-
-    const handleOk = async () => {
-
-       
-        let st = await addCustomer(values)
-        console.log(st)
-        if (st == true) {
-            setConfirmLoading(false)
-            setVisible(false);
-            history.push('/customer')
-
-        }
-
-    };
-
-    const handleCancel = () => {
-        setVisible(false);
-        setConfirmLoading(false)
-    };
     const submitForm = useCallback(
         (values) => {
             showConfirm()
             values.birthdate = values.birthdate._d.getDate() + "/" + (values.birthdate._d.getMonth() + 1) + "/" + values.birthdate._d.getFullYear()
             setValues(values)
-            showModal()
+
+
         },
-        [values, visible]
+        [values]
     )
 
-    
+
 
     return (
         <>
@@ -96,15 +82,6 @@ const CustomerAdd = () => {
                     <CustomInput name="phone_no" label="Phone Number" rule="phoneRequired" rule="required" />
                     <Button type="primary" htmlType="submit"  > Add Customer</Button>
                 </Form>
-                <Modal
-                    title="Title"
-                    visible={visible}
-                    onOk={handleOk}
-                    confirmLoading={confirmLoading}
-                    onCancel={handleCancel}
-                >
-                    <p>confirm</p>
-                </Modal>
             </div>
         </>
     )
